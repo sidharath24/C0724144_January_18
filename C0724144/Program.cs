34cbb6213917425693cdd7b1e04b2511030ac1c8
visualstudio <visualstudio@122-PC14>
2019-01-18 18:25:30 -0500

    My second commit

--- C0724144/Program.cs
+++ C0724144/Program.cs
@@ -10,6 +10,15 @@ namespace C0724144
     {
         static void Main(string[] args)
         {
+            int num,sum=0;
+            for(num=1008;num>=117;num-=81)
+            {
+                sum += num;
+            }
+            Console.WriteLine("The sum of the numbers is: {0}",sum);
+            Console.ReadLine();
+            int i = 1;
+            Console.WriteLine(i);
         }
     }
 }
